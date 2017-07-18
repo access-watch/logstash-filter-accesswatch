407c367c033f60034f824588b3db9be22e7cd28b
Benoît Fleury <me@benfle.com>
2017-07-18 11:58:47 -0500

    First pass at the logstash filter using the API.

--- lib/logstash/filters/accesswatch.rb
+++ lib/logstash/filters/accesswatch.rb
@@ -1,130 +1,136 @@
 # encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
+require 'net/http'
 require 'json'
-require 'set'
-require 'ipaddr'
-require 'interval_tree'
 require 'digest'
+require 'lru_redux'
 
 # The Access Watch filter adds information about robots visiting
 # your website based on data from our robots database.
-#
-# The following fields might be created:
-# [identity][type]      "robot"   If the visitor is a robot.
-# [reputation][status]  string    The reputation of the visitor (see below).
-# [robot][id]           number    A unique robot identifier
-# [robot][name]         string    A robot's name to display to the user.
-# [robot][url]          string    A link to the robot's page on the Access Watch database.
-#
-# Access Watch defines the following reputation statuses:
-#
-# nice	        perfect, as far as we know you can trust this entity
-# ok	          all right, so far no reason to worry about this entity
-# suspicious	  warning, nothing really bad, but the entity is on our radar
-# bad           danger, there is good reasons to watch or block this entity
-#
-# This filter requires the Access Watch `robots.json` file to run.
-#
 
 class LogStash::Filters::Accesswatch < LogStash::Filters::Base
 
   config_name "accesswatch"
 
-  # The path to the Access Watch database file.
-  #
-  # If not specified, this will default to './robots.json'.
-  #
-  config :db_path, :validate => :path, :default => "./robots.json"
+  # Your API Key
+  config :api_key, :validate => :string, :required => true
+
+  # The size of the local cache, 0 to deactivate
+  config :cache_size, :validate => :number, :default => 10000
+
+  # The read timeout for the HTTP request to the service
+  config :read_timeout, :validate => :number, :default => 1
 
   # The field containing the IP address.
   config :ip_source, :validate => :string, :required => true
 
   # The field containing the User-Agent string.
-  config :ua_source, :validate => :string, :required => true
+  config :user_agent_source, :validate => :string, :required => true
 
-  # Transform a CIDR described as a 2-array [start size]
-  # into a Ruby 3-dotted range.
-  private
-  def cidr2range(cidr)
-    first = cidr[0]
-    last = first + cidr[1]
-    (first...last)
-  end
+  # The destination field for address data
+  config :address_destination, :validate => :string
 
-  # Group elements of a collection by each value of a multi-valued attribute
-  private
-  def group_by_multi(coll, key)
-    res = Hash.new {|hash, key| hash[key] = Array.new}
-    coll.each {|el|
-      if !el[key].nil?
-        el[key].each {|val|
-          res[val].push(el)
-        }
-      end
-    }
-    return res
-  end
+  # The destination field for user-agent data
+  config :user_agent_destination, :validate => :string
 
-  private
-  def build_indices(filename)
-    file = File.read(filename)
-    robots = JSON.parse(file)
-    robots.each {|robot|
-      if !robot['cidrs'].nil?
-        robot['cidrs'] = robot['cidrs'].collect {|cidr| cidr2range(cidr)}
-      end
-    }
-    @ip2robots = group_by_multi(robots, 'ips')
-    @cidr2robots = group_by_multi(robots, 'cidrs')
-    @ip2cidrs = IntervalTree::Tree.new(@cidr2robots.keys)
-    @ua2robots = group_by_multi(robots, 'uas')
-  end
+  # The destination field for robot data
+  config :robot_destination, :validate => :string
+
+  # The destination field for reputation data
+  config :reputation_destination, :validate => :string
+
+  @@address_keys = ['value', 'hostname', 'country_code', 'flags']
+  @@robot_keys = ['id', 'name', 'url']
 
   public
   def register
-    build_indices(@db_path)
+    @http_client = Net::HTTP.start('api.access.watch', 80, :read_timeout => @read_timeout)
+    if @cache_size > 0
+      @cache = LruRedux::ThreadSafeCache.new(@cache_size)
+    end
   end
 
-  # Take a User-Agent string and an IP address and return a robot description, or nil.
-  private
-  def detect(ua, ip)
-    # Look for robots with the same IP addressor CIDR
-    ip_candidates = []
-    cidr_candidates = []
-    if ip
-      i = ip.ipv4? ? ip.ipv4_mapped.to_i : ip.to_i # convert IP to arbitrary length integer
-      ip_candidates = @ip2robots[i]
-      cidrs = @ip2cidrs.search(i)
-      cidr_candidates = cidrs.collect {|cidr| @cidr2robots[cidr]}.reduce([], :concat) unless cidrs.nil?
+  def fetch_data(aw_request)
+    begin
+      http_request = aw_request[:http_request]
+      http_request['Api-Key'] = @api_key
+      http_request['Accept'] = 'application/json'
+      http_request['User Agent'] = "Access Watch Logstash Filter Plugin/0.1"
+      http_response = @http_client.request http_request
+      if http_response.code != '200'
+        {:status      => :error,
+         :http_status => http_response.code,
+         :message     => 'AccessWatch: Could not fetch data for this object.'}
+      else
+        {:status => :success,
+         :data   => JSON.parse(http_response.body)}
+      end
+    rescue Net::ReadTimeout
+      {:status => :timeout}
     end
-    # Look for robots with the same User-Agent
-    ua_candidates = []
-    if ua
-      ua_candidates = @ua2robots[Digest::MD5.hexdigest(ua)]
+  end
+
+  def cached_fetch_data(aw_request)
+    if @cache
+      @cache.getset(aw_request[:id]){
+        self.fetch_data(aw_request)
+      }
+    else
+      self.fetch_data(aw_request)
     end
-    # Make a final decision
-    robots = ((ip_candidates | cidr_candidates) & ua_candidates)
-    if !robots.empty?
-      robot = robots[0]
-      url = "https://access.watch/database/robots/#{robot['reputation']}/#{robot['urlid'] or robot['id']}"
-      {'identity'   => {'type' => 'robot'},
-       'robot'      => {'id'   => robot['id'],
-                        'name' => robot['name'],
-                        'url'  => url},
-       'reputation' => {'status' => robot['reputation']}}
+  end
+
+  def fetch_address(ip)
+    self.cached_fetch_data({:id           => "ip-#{ip}",
+                            :http_request => Net::HTTP::Get.new("/1.1/address/#{ip}")})
+  end
+
+  def fetch_user_agent(user_agent)
+    http_request = Net::HTTP::Post.new('/1.1/user-agent')
+    http_request.body = JSON.generate({:value => user_agent})
+    http_request.content_type = 'application/json'
+    id = "ua-#{Digest::MD5.hexdigest(user_agent)}"
+    self.cached_fetch_data({:id           => id,
+                            :http_request => http_request})
+  end
+
+  def fetch_identity(ip, user_agent)
+    ip = ip || ''
+    user_agent = user_agent || ''
+    http_request = Net::HTTP::Post.new('/1.1/identity')
+    http_request.body = JSON.generate({:address => ip,
+                                       :user_agent => user_agent})
+    http_request.content_type = 'application/json'
+    id = "identity-#{Digest::MD5.hexdigest(ip)}-#{Digest::MD5.hexdigest(user_agent)}"
+    self.cached_fetch_data({:id           => id,
+                            :http_request => http_request})
+  end
+
+  def augment(event, destination, data, keys=nil)
+    if destination && data
+      event.set(destination,
+                data.select {|k, v|
+                  (keys.nil? or keys.include?(k)) && !(v.nil? || v.empty?)
+                })
     end
   end
 
   public
   def filter(event)
-    ip_s = event.get(@ip_source)
-    ip = IPAddr.new ip_s unless ip_s.nil?
-    robot = detect(event.get(@ua_source), ip)
-    if robot
-      event.set('identity',   robot['identity'])   unless robot['identity'].nil?
-      event.set('robot',      robot['robot'])      unless robot['robot'].nil?
-      event.set('reputation', robot['reputation']) unless robot['reputation'].nil?
+    ip = event.get(@ip_source)
+    user_agent = event.get(@user_agent_source)
+    if @ip_source and @user_agent_source
+      data = self.fetch_identity(ip, user_agent)
+      self.augment(event, @address_destination, data[:address], @@address_keys)
+      self.augment(event, @robot_destination, data[:robot], @@robot_keys)
+      self.augment(event, @reputation_destination, data[:reputation])
+    elsif @ip_source
+      data = self.fetch_address(ip)
+      self.augment(event, @address_destination, data, @@address_keys)
+    else
+      data = self.fetch_user_agent(user_agent)
+      self.augment(event, @user_agent_destination, data)
     end
     filter_matched(event)
   end
--- logstash-filter-accesswatch.gemspec
+++ logstash-filter-accesswatch.gemspec
@@ -1,10 +1,10 @@
 # coding: utf-8
 Gem::Specification.new do |s|
   s.name          = 'logstash-filter-accesswatch'
-  s.version       = '0.1.0'
+  s.version       = '0.2.0'
   s.licenses      = ['Apache-2.0']
   s.summary       = 'The Logstash filter plugin for Access Watch (http://access.watch).'
-  s.description   = 'The Access Watch filter adds information about robots visiting your website based on data from our robots database.'
+  s.description   = 'The Access Watch filter adds information about robots visiting your website based on data from our robot database.'
   s.homepage      = 'http://access.watch'
   s.authors       = ['Benoît Fleury']
   s.email         = 'benoit@access.watch'
@@ -12,8 +12,6 @@ Gem::Specification.new do |s|
 
   # Files
   s.files = Dir['lib/**/*','spec/**/*','vendor/**/*','*.gemspec','*.md','Gemfile','LICENSE']
-   # Tests
-  s.test_files = s.files.grep(%r{^(test|spec|features)/})
 
   # Special flag to let us know this is actually a logstash plugin
   s.metadata = { 'logstash_plugin' => 'true',
@@ -21,7 +19,7 @@ Gem::Specification.new do |s|
 
   # Gem dependencies
   s.add_runtime_dependency 'logstash-core-plugin-api', '~>   2.0'
-  s.add_runtime_dependency 'augmented_interval_tree',  '~> 0.1.1'
+  s.add_runtime_dependency 'lru_redux', '~> 1.1'
 
   s.add_development_dependency 'logstash-devutils', '1.3.3'
 end
